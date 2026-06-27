9cc57b1467e94193d3733a03596753de9a49041d
Pavan Lanka <PavanMCA2011@Gmail.Com>
2026-06-27 10:51:22 +0530

    Documentations and Default Arguments and Global vs Local Variables

---
+++ Python Practice/Dictionaries and Tuples.py
@@ -0,0 +1,33 @@
+#Dictionaries = Key - Value /Maps/HashTables/Associate Arrays
+#d = {"tom":2136546553,"rob":655462465,"joe":51654656165}
+# print(d)
+# print(d["tom"])
+# d["sam"] = 54564654646
+# print(d)
+# del d["sam"]
+
+# for key in d:
+#     print(key)
+#     print(d[key])
+
+# for k,v in d.items():
+#     print(k)
+#     print(v)
+
+#"tom" in d
+#returns true or false
+# d.clear()
+# print(d)
+
+#Tuple is a list of values grouped together
+#Here List will always carry same data types (Homogeneous)
+#Tuple may or may not carry same data types (Heterogeneous)
+#Tuple does not support  item or record assignment  same like List or Dict they are immutable
+
+# point = (5,9)
+# print(point[0])
+# print(point[1])
+
+address = ("sdfasfd","sdfasdfdasfa",505152, 5.50)
+print(address)
+
