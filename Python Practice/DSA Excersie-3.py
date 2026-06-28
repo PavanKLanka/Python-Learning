0ae7a6f29818870296f97d44c62b2ad761591fa2
Pavan Lanka <PavanMCA2011@Gmail.Com>
2026-06-28 10:21:05 +0530

    Exercise - 3

--- Python Practice/DSA Excersie-3.py
+++ Python Practice/DSA Excersie-3.py
@@ -1,15 +1,26 @@
 # Create a list of all odd numbers between 1 and a max number. Max number is something you need to take from a user using input() function
+# try:
+#     max_input_number = int(input("Enter Max Number here: "))
+#     staring_odd_number = 1
+#     odd_numbers_list = []
+#     while staring_odd_number <= max_input_number:
+#         if staring_odd_number % 2 != 0:
+#             odd_numbers_list.append(staring_odd_number)
+#             staring_odd_number = 1 + staring_odd_number
+#         else:
+#             staring_odd_number = 1 + staring_odd_number
+#             continue
+#     print(odd_numbers_list)
+# except ValueError as e:
+#     print(type(e).__name__)
+
+# Another approach
 try:
-    max_input_number = int(input("Enter Max Number here: "))
-    staring_odd_number = 1
-    odd_numbers_list = []
-    while staring_odd_number <= max_input_number:
-        if staring_odd_number % 2 != 0:
-            odd_numbers_list.append(staring_odd_number)
-            staring_odd_number = 1 + staring_odd_number
-        else:
-            staring_odd_number = 1 + staring_odd_number
-            continue
-    print(odd_numbers_list)
+    max_numbers = int(input("Enter Max Number here: "))
+    odd_list = []
+    for number in range(1, max_numbers):
+        if number % 2 != 0:
+            odd_list.append(number)
+    print(odd_list)
 except ValueError as e:
     print(type(e).__name__)
